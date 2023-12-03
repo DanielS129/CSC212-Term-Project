6ef48c9fe5eaf29a821f7b8b42fe0629281d4d76
DanielS129 <111005845+DanielS129@users.noreply.github.com>
2023-12-03 20:59:45 +0000

    Adding Function Declarations and Implementations

---
+++ Code/btNode.cpp
@@ -0,0 +1,32 @@
+#include "btNode.h"
+
+/*
+    BTNode Class Implementation 
+    
+    Public Functions
+*/
+
+BTNode :: BTNode(int degree, bool isleaf){
+    this->DEGREE = degree;
+    this->isLeaf = isleaf;
+    this->num = 0;
+
+    // Allocate memory for keys and children pointers
+    keys = new std::string[2 * DEGREE - 1]; // Max keys in a node is 2*t - 1
+    children = new BTNode*[2 * DEGREE]; // Max children is 2*t
+    
+    // Initialize children pointers to nullptr
+    for (int i = 0; i < 2 * DEGREE; ++i) {
+        children[i] = nullptr;
+    }
+
+}
+
+BTNode :: ~BTNode(){
+    delete[] keys; // Deallocate keys array
+    // Recursively delete child nodes
+    for (int i = 0; i <= num; ++i) {
+        delete children[i];
+    }
+    delete[] children; // Deallocate children pointers array
+}
--- Code/btNode.h
+++ Code/btNode.h
@@ -22,6 +22,9 @@ class BTNode{
         std::string *keys; // Will represent the list of the "Data"
         BTNode **children; // Will represent a list of Node Pointers.
         bool isLeaf; // Is true when node is leaf. Otherwise false
-
+        
+        friend class BTree;
     public:
+        BTNode(int degree, bool isleaf);  
+        ~BTNode();
 };
