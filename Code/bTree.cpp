811383fcf0dff8ab12ab3693a050326382c36d93
danieltran247 <90417943+danieltran247@users.noreply.github.com>
2023-12-03 22:57:53 -0500

    Update bTree.cpp

--- Code/bTree.cpp
+++ Code/bTree.cpp
@@ -1,13 +1,31 @@
 #include "bTree.h"
 
 BTree::BTree() {
+  root = nullptr;
+}
+
+BTree::BTree() {
 }
 
 void BTree::insert(const std::string& key) {
+  if (!root) {
+    root = new BTNode(true);
+  }
+  // insertion logic
 }
 
 void BTree::remove(const std::string& key) {
+  if (!root) {
+    return;
+  }
+  // removal logic
 }
 
 void BTree::print(int mode) {
+  if (!root) {
+    return;
+  }
+
+
 }
+
