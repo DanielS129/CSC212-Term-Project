44a58a720175b499d3f80b629ae11068b547e219
mithilesh <113184024+MithileshPabbaa@users.noreply.github.com>
2023-12-04 17:04:24 -0500

    Update btNode.cpp

--- Code/btNode.cpp
+++ Code/btNode.cpp
@@ -13,6 +13,12 @@ BTNode :: BTNode(int degree, bool isleaf){
 
     // Allocate memory for keys and children pointers
     keys = new std::string[2 * DEGREE - 1]; // Max keys in a node is 2*t - 1
+
+    // Initialize keys to empty strings
+    for (int i = 0; i < 2 * DEGREE - 1; ++i) {
+        keys[i] = std::string();
+    }
+    
     children = new BTNode*[2 * DEGREE]; // Max children is 2*t
     
     // Initialize children pointers to nullptr
