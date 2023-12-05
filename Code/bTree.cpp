93e136aff777964d8185c371c1d5f58da4dcd361
DanielS129 <111005845+DanielS129@users.noreply.github.com>
2023-12-05 00:12:52 +0000

    Updated the insertR Function

--- Code/bTree.cpp
+++ Code/bTree.cpp
@@ -4,49 +4,25 @@
 
 // Private Functions
 
+//Function for insertion recursively + Finsih Split Node Function
 void BTree::insertR(BTNode* node, const std::string& key) {
-  // If the node is a leaf, insert the key directly into the node
-    if (node->isLeaf) {
-        int i;
-        for (i = node->num - 1; (i >= 0 && node->keys[i] > key); i--) {
-            node->keys[i + 1] = node->keys[i]; // Shift keys to the right
-        }
-        node->keys[i + 1] = key; // Insert the new key
-        node->num++; // Increment the number of keys in the node
-    } else {
-        // If the node is not a leaf, find the child which is going to have the new key
-        int i = node->num - 1;
-        while (i >= 0 && node->keys[i] > key) {
-            i--;
-        }
-        // Check if the found child is full
-        if (node->children[i + 1]->num == 2 * this->Degree - 1) {
-            // If the child is full, then split it
-            BTNode* child = node->children[i + 1];
-            BTNode* newChild = new BTNode(Degree, child->isLeaf);
-
-            node->keys.insert(node->keys.begin() + i + 1, child->keys[Degree - 1]);
-            newChild->keys.assign(child->keys.begin() + Degree, child->keys.end());
-            child->keys.erase(child->keys.begin() + Degree - 1, child->keys.end());
-
-            if (!child->isLeaf) {
-                newChild->children.assign(child->children.begin() + Degree, child->children.end());
-                child->children.erase(child->children.begin() + Degree, child->children.end());
-            }
-
-            node->num++;
-            child->num = Degree - 1;
-            node->children.insert(node->children.begin() + i + 2, newChild);
-          
-            // After splitting, decide where to insert the new key
-            if (node->keys[i + 1] < key) {
-                i++;
-            }
-        }
-        insertR(node->children[i + 1], key); // Recursively insert the key in the appropriate child
+  int i = 0;
+  if (node->isLeaf) {
+    // If node is a leaf, insert key into this node
+    node->keys.push_back(key);
+  }
+  else {
+    // Find child node to insert to   
+    while (i < node->keys.size() && key > node->keys[i]) {
+      i++;
     }
-
-
+    insertR(node->children[i], key);
+    // Handle the case when the child node is full
+    if (node->children[i]->keys.size() > MAX_KEYS) {
+      
+    }
+  }
+}
 
 bool BTree::searchR(BTNode* node, const std::string& key) {
   int i = 0;
@@ -103,7 +79,7 @@ bool BTree::search(const std::string& key) {
   return searchR(root, key);
 }
 
-void BTree::print(int mode) {
+void BTree::print() {
   if (!root) {
     return;
   }
