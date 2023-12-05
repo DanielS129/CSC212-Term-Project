9346b7df39922d8476d699049c9c2c16e103bf62
jayfernnycode <123682667+jayfernnycode@users.noreply.github.com>
2023-12-04 19:14:46 -0500

    Updating main.cpp

--- Code/main.cpp
+++ Code/main.cpp
@@ -8,7 +8,6 @@ void print_tree(BTree* tree, int mode);
 
 // Providing a general layout of main.cpp
 
-// <file_name> <traversal_method>
 int main(int argc, char*argv[]){
     std::ifstream ifs(argv[1]);
     int mode = std::stoi(argv[2]);
